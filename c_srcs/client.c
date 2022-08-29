18fbc5a3ea9a18e4e4cc4749cb789655f4ed437c
chanma <yunosuke.homma@icloud.com>
2022-08-29 23:17:17 +0900

    add error message

--- c_srcs/client.c
+++ c_srcs/client.c
@@ -1,13 +1,22 @@
 #include "../includes/minitalk.h"
 
+void	display_error(int num)
+{
+	if (num == USAGE)
+		ft_putendl_fd("\tusage: ./client [pid] [\"string\"]", 2);
+	else if (num == ERROR)
+		ft_putendl_fd("Error", 2);
+	exit(EXIT_FAILURE);
+
 int main(int argc, char **argv)
 {
 	int		s_pid;
 	size_t	i;
 	size_t	bit;
+	int		ret_kill;
 
 	if (argc != 3)
-		return (0);
+		display_error(USAGE);
 	s_pid = ft_atoi(argv[1]);
 	i = 0;
 	while (i < ft_strlen(argv[2]))
@@ -16,9 +25,11 @@ int main(int argc, char **argv)
 		while (bit < CHAR_BIT)
 		{
 			if (argv[2][i] >> bit & 1)
-				kill(s_pid, SIGUSR1);
+				ret_kill = kill(s_pid, SIGUSR1);
 			else
-				kill(s_pid, SIGUSR2);
+				ret_kill = kill(s_pid, SIGUSR2);
+			if (ret_kill == -1)
+				display_error(ERROR);
 			bit++;
 			usleep(500);
 		}
