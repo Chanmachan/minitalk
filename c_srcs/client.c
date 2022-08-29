55e8982d50059d27f84eeabae2bfff79676a1d30
chanma <yunosuke.homma@icloud.com>
2022-08-29 19:26:24 +0900

    almost

--- c_srcs/client.c
+++ c_srcs/client.c
@@ -4,22 +4,31 @@ int main(int argc, char **argv)
 {
 	int		s_pid;
 	size_t	i;
-	size_t	j;
+	size_t	bit;
 
-	j = 0;
-	if (argc <= 2)
+	if (argc != 3)
+		return (0);
+	s_pid = ft_atoi(argv[1]);
+	i = 0;
+	while (i < ft_strlen(argv[2]))
 	{
-		s_pid = ft_atoi(argv[1]);
-		i = 0;
-		while (i < ft_strlen(argv[2]))
+		bit = 0;
+		while (bit < CHAR_BIT)
 		{
-			while (j < 8)
-			{
-				kill(s_pid, argv[2][j]);
-				j++;
-			}
-			i++;
+			if (argv[2][i] >> bit & 1)
+				kill(s_pid, SIGUSR1);
+			else
+				kill(s_pid, SIGUSR2);
+			bit++;
 		}
+		i++;
 	}
 	return (0);
 }
+
+/*int main(int argc, char **argv)
+{
+	(void)argc;
+	pid_t s_pid = ft_atoi(argv[1]);
+	kill(s_pid, ft_atoi(argv[2]));
+}*/
--- s_srcs/server.c
+++ s_srcs/server.c
@@ -1,25 +1,39 @@
 #include "../includes/minitalk.h"
 
+volatile sig_atomic_t	g_receive_sig;
+int bit = 0;
+int ret = 0;
+
+void	output_char()
+{
+	if (g_receive_sig == SIGUSR1)
+		ret += 0b00000001 << bit;
+	if (bit == 7)
+	{
+		ft_putchar_fd(ret, 1);
+		bit = 0;
+		ret = 0;
+	}
+	bit++;
+	g_receive_sig = 0;
+}
+
 void	signal_handler(int signum)
 {
-	(void)signum;
+	g_receive_sig = signum;
 }
 
 int main()
 {
-	int	s_pid;
+	int			s_pid;
 
 	s_pid = getpid();
-	printf("%d\n", s_pid);
-
-	int a = 10;
-	int i = 1;
-	int digit = 0;
-	while (i < a)
-	{
-		printf("%d [%d]\n", a>>digit & 1, digit);
-		digit++;
-		i = i * 2;
-	}
+	printf("pid : %d\n", s_pid);
 	signal(SIGUSR1, signal_handler);
+	signal(SIGUSR2, signal_handler);
+	while (1)
+	{
+		pause();
+		output_char();
+	}
 }
